19fc8237a6f1b976e823426dbe6d7b83ba2bda8f
mateopuntousg <mpuntousg@gmail.com>
2026-04-05 22:57:27 -0400

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production', // cambiar de 'development' a 'production'
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/Magda-web/', 
     clean: true,
   },
   module: {
