c6fff6d1759ea998a351dedd83ea11812539898f
mpuntous <mateo.puntous@uc.cl>
2026-04-08 18:12:18 -0400

    Fix publicPath for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env, argv) => {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
-      publicPath: '/',
+      publicPath: '/PlasticaWorking/',
       clean: true,
     },
     devServer: {
